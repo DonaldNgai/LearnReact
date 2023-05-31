2a12a253e52c01e4b0c148ce5f20af5326c46cd7
Donald Ngai <donaldptngai@gmail.com>
2023-05-31 12:24:22 -0600

    Adding all files

---
+++ public/index.js
@@ -0,0 +1,32 @@
+let quotesDiv = document.getElementById('quotes')
+
+fetch('https://api.kanye.rest')
+	.then(
+		res => res.json()
+	).then(
+		quote => {
+			quotesDiv.innerHTML += '<p> ${quote.quote} </p>'
+		}
+	);
+
+let catButton = document.getElementById('give-cat')
+
+catButton.addEventListener("click", evt=> {
+
+	let catDiv = document.getElementById('cat-pic')
+
+	fetch('https://api.thecatapi.com/v1/images/search?')
+	.then(
+		res => res.json()
+	).then(
+		cats => {
+			cats.forEach( cat => {
+				catDiv.innerHTML += `<p> Here's a cat </p> 
+				<img src="$(cat.url)" alt="kitty">`
+			});.
+			
+		}
+	);
+
+});
+
