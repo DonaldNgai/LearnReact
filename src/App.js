460806e330226935c3488e8080cf25a1c8438b4e
Donald Ngai <donaldptngai@gmail.com>
2023-07-10 20:59:27 -0400

    adding logic to display images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import {useCallback} from 'react';
+import {useCallback, useState} from 'react';
 import logo from './logo.svg'; // Give logo a link from a variable
 import './App.css';
 
 // Import the useDropzone hooks from react-dropzone
-import { useDropzone } from "react-dropzone";
+import { useDropzone } from "react-dropzone"
 // Import cuid which allows easy Unique ID generation
 import cuid from "cuid"
 
 let acceptedFormats = 'application/pdf, image/*';
+// Create a state called images using useState hooks and pass the initial value as empty array
+// States are important in react as it tells when the DOM model gets
+// Re-rendered. It rerenders when a state changes
+
+
+const Image = ({ image }) => {
+    return (
+        <div className="file-item">
+            <img alt={'img - ${image.id}'} src={image.src} className="file-img" />
+        </div>
+    );
+};
+
+const ImageList = ({ images }) => {
+      // render each image by calling Image component
+    const renderImage = ( image, index ) => {
+        return(
+            <Image image={image} key={'${image.id}-image'}/>
+        );
+    };
+
+    // Return the list of files
+    return (
+        <section className="file-list">
+            {images.map(renderImage)}
+        </section>
+    );
+};
 
 const Dropzone = ({ onDrop, accept }) => {
     // Initializing useDropzone hooks with options
@@ -38,20 +66,35 @@ const Dropzone = ({ onDrop, accept }) => {
 
 };
 
-function handleOnDrop(acceptedFiles)
+function handleOnDrop(acceptedFiles, images, setImages)
 {
     acceptedFiles.map(file => {
         // Initialize FileReader Browser API
         const reader = new FileReader();
-        
-    });
+        // This callback function gets called
+        // after the file is read
+        reader.onload = function (e)
+        {
+            // add the image into the state. 
+            // Since FileReader reading process is asynchronous, 
+            // its better to get the latest snapshot state (i.e., prevState) 
+            // and update it. 
+            setImages(prevState => [ ...prevState, { id: cuid(), src: e.target.result }]);
+            
+            // Read the file as Data URL (since we accept only images)
+            reader.readAsDataURL(file);
+            return file;
+        };
+    }, []);
 }
 
 function DropAudioApp(){
+    const [images, setImages] = useState([]); 
+
     const onDrop = useCallback(acceptedFiles => {
         // this callback will be called after files get dropped, we will get the acceptedFiles. If you want, you can even access the rejected files too
         console.log(acceptedFiles);
-        handleOnDrop(acceptedFiles);
+        handleOnDrop(acceptedFiles, images, setImages);
     }, []);
 
     // We pass onDrop function and accept prop to the component. It will be used as initial params for useDropzone hook
@@ -59,6 +102,7 @@ function DropAudioApp(){
         <main className="App">
             <h1 className="text-center">Drag and Drop Example</h1>
             <Dropzone onDrop={onDrop} accept={acceptedFormats} />
+            <ImageList images={images} />
         </main>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import ReceiveAudioApp from './App'; // Imports App.js and css and stuff
